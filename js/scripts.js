01dd5b45441a184bbf4897ebe900e3c0ce54262d
Esvethlynna Pangelinan <esveth@aol.com>
2017-06-02 12:57:26 -0700

    add branching code to determine the result based on user's selections

--- js/scripts.js
+++ js/scripts.js
@@ -6,45 +6,95 @@ $(document).ready(function(){
   var trait = parseInt($("#trait").val());
   var activity = parseInt($("#activity").val());
 
-  $(".important-group").change(function() {
-    important = parseInt($("#important").val());
-    alert(important);
-  });
-
-  $(".appType-group").change(function() {
-    appType = parseInt($("#appType").val());
-    alert(appType);
-  });
-
-  $(".company-group").change(function() {
-    company = parseInt($("#company").val());
-    alert(company);
-  });
-
-  $(".trait-group").change(function() {
-    trait = parseInt($("#trait").val());
-    alert(trait);
-  });
-
-  $(".activity-group").change(function() {
-    activity = parseInt($("#activity").val());
-    alert(activity);
-  });
-
-
-
   var css_track = 0;
   var java_track = 0;
   var c_track = 0;
 
+  var result = "undecided";
+
+  $(".important-group").change(function() {
+    important = parseInt($("#important").val());
+    //alert(important);
+  });
+
+  $(".appType-group").change(function() {
+    appType = parseInt($("#appType").val());
+    //alert(appType);
+  });
+
+  $(".company-group").change(function() {
+    company = parseInt($("#company").val());
+    //alert(company);
+  });
+
+  $(".trait-group").change(function() {
+    trait = parseInt($("#trait").val());
+    //alert(trait);
+  });
+
+  $(".activity-group").change(function() {
+    activity = parseInt($("#activity").val());
+    //alert(activity);
+  });
+
   $('form').submit(function(event) {
     event.preventDefault();
-    alert("submitted the form");
+    if (important === 1) {
+      css_track += 1;
+    } else if (important === 2) {
+      java_track += 1;
+    } else if (important === 3){
+      c_track += 1;
+    }
+
+    if (appType === 1) {
+      css_track += 1;
+    } else if (appType === 2) {
+      java_track += 1;
+    } else if (appType === 3){
+      c_track += 1;
+    }
+
+    if (company === 1) {
+      css_track += 1;
+    } else if (company === 2) {
+      java_track += 1;
+    } else if (company === 3){
+      c_track += 1;
+    }
+
+    if (trait === 1) {
+      css_track += 1;
+    } else if (trait === 2) {
+      java_track += 1;
+    } else if (trait === 3){
+      c_track += 1;
+    }
+
+    if (activity === 1) {
+      css_track += 1;
+    } else if (activity === 2) {
+      java_track += 1;
+    } else if (activity === 3){
+      c_track += 1;
+    }
+
+    //alert("submitted the form");
+    $('form').hide();
     $(".result").show();
-    alert(important);
-    alert (appType);
-    alert(company);
-    alert (trait);
-    alert (activity);
+
+    if (css_track >=3) {
+      result = "CSS/Design";
+    } else if (java_track >=3) {
+      result = "Java/Android";
+    } else if (c_track >=3) {
+      result = "C#/.NET";
+    }
+
+    $(".bestTrack").text(result);
+    //alert("css:" + css_track);
+    //alert ("java:" + java_track);
+    //alert("c:" + c_track);
+    //alert("result is " + result)
   });
 });
